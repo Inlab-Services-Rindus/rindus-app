922c0819869b45ab6621fe36e80ad3fe80ad1728
Adrian Gonzalez <adrian.gonzalez@rindus.de>
2023-10-12 16:32:31 +0200

    main: Updated backend URL

--- frontend/src/config/environments/production.ts
+++ frontend/src/config/environments/production.ts
@@ -1,5 +1,5 @@
 import { AppConfig } from '@/model/Config';
 
 export const prodConfig = (): AppConfig => ({
-  backendUrl: 'https://warwick-headless-chicken-monster-mjxp.2.ie-1.fl0.io',
+  backendUrl: 'https://warwick-headless-chicken-monster-mjxp.1.ie-1.fl0.io',
 });
