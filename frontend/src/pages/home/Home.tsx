3049b7606539152277920de84d7ed5106175e28d
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-10-19 10:40:42 +0200

    RA-43: Delete unsued import

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,7 +2,6 @@ import { config } from '@/config/config';
 import useFetch from '@/hooks/fetch/useFetch';
 import useToast from '@/hooks/toast/useToast';
 import { Employee } from '@/model/Employee';
-import { Header } from '@/organisms/header/Header';
 import { PeopleTab } from '@/organisms/people-tab/PeopleTab';
 import Retry from '@/organisms/retry/Retry';
 
