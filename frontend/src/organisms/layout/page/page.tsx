6f31d4e0cf00ad259efd6073b37c9ced501519c2
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-10-19 18:47:51 +0200

    RA-43: Fix css login

--- frontend/src/organisms/layout/page/page.tsx
+++ frontend/src/organisms/layout/page/page.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 
 import { Header } from '@/organisms/header/Header';
+import '@/organisms/layout/page/Page.scss';
 
 interface Props {
   children: ReactNode;
@@ -8,7 +9,7 @@ interface Props {
 
 export default function Page({ children }: Props) {
   return (
-    <div>
+    <div className="page">
       <Header />
       <main>{children}</main>
     </div>
