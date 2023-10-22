fdf094fa7b3b6f34319ca05926d8941bcd7c71ae
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-10-22 14:37:43 +0200

    RA-44: Change theme

--- frontend/manifest.ts
+++ frontend/manifest.ts
@@ -2,21 +2,19 @@ import { VitePWAOptions } from 'vite-plugin-pwa';
 
 export const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'autoUpdate',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  includeAssets: ['rindusLogo.svg'],
   manifest: {
     name: 'Rindus App',
     short_name: 'Rindus App',
     description: 'Rindus app for employees.',
     icons: [
       {
-        src: 'rindusLogo.svg',
+        src: '/rindusLogo.svg',
         sizes: '48x48 72x72 96x96 128x128 256x256 512x512',
         type: 'image/svg+xml',
         purpose: 'any',
       },
     ],
-    theme_color: 'red',
-    background_color: 'blue',
     display: 'standalone',
     scope: '/',
     start_url: '/',
