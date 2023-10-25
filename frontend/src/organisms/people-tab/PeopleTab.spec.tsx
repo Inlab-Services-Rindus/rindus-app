ff67402b0ee0df2b5693e2459772933863097f99
aridane.martin_ext.douglas.eu <aridane.m.falcon@rindus.de>
2023-10-25 12:57:32 +0100

    RA-42: Remove test that relates to another component

--- frontend/src/organisms/people-tab/PeopleTab.spec.tsx
+++ frontend/src/organisms/people-tab/PeopleTab.spec.tsx
@@ -1,7 +1,7 @@
 import { mockUsersResponse } from '@/model/__mocks__/fetch/Employee';
 import { PeopleTab } from '@/organisms/people-tab/PeopleTab';
 
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 describe('PeopleTab', () => {
   it('renders a list of people', () => {
@@ -16,21 +16,5 @@ describe('PeopleTab', () => {
     expect(
       screen.getByText(mockUsersResponse[0].firstName),
     ).toBeInTheDocument();
-  });
-
-  it('calls refreshPeople when retry button is clicked', () => {
-    const mockRefreshPeople = vi.fn();
-    render(
-      <PeopleTab
-        people={[]}
-        isPeopleLoading={true}
-        refreshPeople={mockRefreshPeople}
-      />,
-    );
-    const refreshButton = screen.getByTestId('refresh-button');
-
-    fireEvent.click(refreshButton);
-
-    expect(mockRefreshPeople).toHaveBeenCalled();
   });
 });
