cc075a8efff55d39656bf594e904b7aba6c307e6
Adrian Gonzalez <adrian.gonzalez@rindus.de>
2023-10-31 12:41:28 +0100

    Now project can be started with real data if supplied

--- backend/seeds/helper/index.ts
+++ backend/seeds/helper/index.ts
@@ -4,14 +4,14 @@ import { PersonioData } from '@/models/service/seeds/Personio';
 
 const RESOURCES_PATH = './seeds/resources';
 export const USERS_FILE_NAME = `${RESOURCES_PATH}/users.json`;
-export const PARTNERS_FILE_NAME = `${RESOURCES_PATH}/partners.json`;
-
-export function parseFromSeedJSONFile<T>(fileName: string): T[] {
-  return parseJSON(fileName);
-}
+export const PERSONIO_USERS_FILE_NAME = `${RESOURCES_PATH}/personio-users.json`;
 
 export function parsePersonioJSONFile(): PersonioData {
-  return parseJSON(USERS_FILE_NAME);
+  if (fs.existsSync(PERSONIO_USERS_FILE_NAME)) {
+    return parseJSON(PERSONIO_USERS_FILE_NAME);
+  } else {
+    return parseJSON(USERS_FILE_NAME);
+  }
 }
 
 function parseJSON<T>(fileName: string): T {
