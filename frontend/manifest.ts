a08dee58597253c4a1577dee57e0ff194ff47ff7
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-11-04 16:01:55 +0100

    RA-44: New config

--- frontend/manifest.ts
+++ frontend/manifest.ts
@@ -12,13 +12,11 @@ export const manifestForPlugin: Partial<VitePWAOptions> = {
         src: '/LOGO_Size_192px.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'maskable',
       },
       {
         src: '/LOGO_Size_512px.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'maskable',
       },
       {
         src: '/LOGO_Size_180px.png',
