2d142ba455946bb0e913dab3703b7fe63cbdc0f7
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-11-06 19:17:06 +0100

    RA-59: Add log

--- frontend/src/config/environments/production.ts
+++ frontend/src/config/environments/production.ts
@@ -2,6 +2,9 @@ import { AppConfig } from '@/model/Config';
 
 const backendUrl = import.meta.env.BACKEND_URL;
 
+console.log('Pedro ===> backendUrl', backendUrl);
+
+
 export const prodConfig = (): AppConfig => ({
   backendUrl,
 });
