13871982eaff1d1b531731e47a8ebb06fd0a4b39
Adrian Gonzalez <adrian.gonzalez@rindus.de>
2023-11-07 15:52:02 +0100

    Failing build fixed

--- backend/src/repository/__mocks__/partner.ts
+++
@@ -1,8 +0,0 @@
-import { Partner } from '@/models/business/Partner';
-import { PartnerRepository } from '@/repository/partner.repository';
-
-export const mockAll = jest.fn().mockReturnValue([]);
-
-export class MockPartnerRepository implements PartnerRepository {
-  all: () => Promise<Partner[]> = mockAll;
-}
