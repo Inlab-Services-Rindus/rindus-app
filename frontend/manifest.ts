c92f4b5de1ec74e2ff8b2acc7eaab78974e4c5bc
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-11-16 13:06:21 +0100

    RA-72: Add new images for pwa

--- frontend/manifest.ts
+++ frontend/manifest.ts
@@ -2,8 +2,6 @@ import { VitePWAOptions } from 'vite-plugin-pwa';
 
 export const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'autoUpdate',
-  includeAssets: ['/LOGO_Size_192px.png'],
-
   manifest: {
     name: 'Rindus',
     short_name: 'Rindus',
@@ -11,30 +9,30 @@ export const manifestForPlugin: Partial<VitePWAOptions> = {
     description: 'Rindus app for employees.',
     icons: [
       {
-        src: '/LOGO_Size_192px.png',
+        src: '/LOGO_Size_192px_square.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/LOGO_Size_512px.png',
+        src: '/LOGO_Size_512px_square.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: '/LOGO_Size_180px.png',
+        src: '/LOGO_Size_180px_square.png',
         sizes: '180x180',
         type: 'image/png',
         purpose: 'apple touch icon',
       },
 
       {
-        src: '/LOGO_Size_512px.png',
+        src: '/LOGO_Size_512px_square.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/LOGO_Size_512px.png',
+        src: '/LOGO_Size_512px_square.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
