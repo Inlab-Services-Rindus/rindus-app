a5da36719bcaaf32b895c0762ea86bc37bf56232
Pedro Domínguez Aguilera <pedro.dominguez@rindus.de>
2023-11-16 16:56:03 +0100

    RA-72: fix google button

--- frontend/src/ui/components/atoms/buttons/google/GoogleButton.tsx
+++ frontend/src/ui/components/atoms/buttons/google/GoogleButton.tsx
@@ -1,24 +1,27 @@
-import '@/ui/components/atoms/buttons/google/GoogleButton.scss';
+import { useEffect } from 'react';
 
 export interface GoogleButtonProps {
   afterLogin: (token: google.accounts.id.CredentialResponse) => void;
 }
 
-export default function GoogleButton({
+export default function GoogleButto({
   afterLogin,
 }: GoogleButtonProps): JSX.Element {
-  function handleGoogleLogin() {
+  useEffect(() => {
     google.accounts.id.initialize({
       callback: afterLogin,
       client_id:
         '794492959607-21m9v38tca8f0i957p9bk67li2g7nt9b.apps.googleusercontent.com',
     });
-    google.accounts.id.prompt();
-  }
 
-  return (
-    <button className="google__button" onClick={handleGoogleLogin}>
-      Sign in with Google
-    </button>
-  );
+    const div = document.getElementById('google__button') as HTMLElement;
+
+    google.accounts.id.renderButton(div, {
+      type: 'standard',
+      shape: 'pill',
+      size: 'large',
+    });
+  }, []);
+
+  return <div id="google__button" />;
 }
