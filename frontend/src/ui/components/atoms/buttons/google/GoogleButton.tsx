8fd2e0419bfdc50b6f220a5f8a3b0a52d3dcd6d8
Pistolero3 <antonio.morales@rindus.de>
2023-11-17 13:33:23 +0100

    RA-73: solving google Library loading

--- frontend/src/ui/components/atoms/buttons/google/GoogleButton.tsx
+++ frontend/src/ui/components/atoms/buttons/google/GoogleButton.tsx
@@ -8,20 +8,22 @@ export default function GoogleButto({
   afterLogin,
 }: GoogleButtonProps): JSX.Element {
   useEffect(() => {
-    google?.accounts?.id?.initialize({
-      callback: afterLogin,
-      client_id:
-        '794492959607-21m9v38tca8f0i957p9bk67li2g7nt9b.apps.googleusercontent.com',
-    });
+    if (window?.google) {
+      window?.google?.accounts?.id?.initialize({
+        callback: afterLogin,
+        client_id:
+          '794492959607-21m9v38tca8f0i957p9bk67li2g7nt9b.apps.googleusercontent.com',
+      });
+    }
 
     const div = document.getElementById('google__button') as HTMLElement;
 
-    google?.accounts?.id?.renderButton(div, {
+    window?.google?.accounts?.id?.renderButton(div, {
       type: 'standard',
       shape: 'pill',
       size: 'large',
     });
-  }, [google?.accounts?.id]);
+  }, []);
 
   return <div id="google__button" />;
 }
