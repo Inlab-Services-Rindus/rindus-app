5b63901b3574d31d3df8612e916c63a6502b3743
Adrian Gonzalez <adrian.gonzalez@rindus.de>
2023-11-29 12:19:11 +0100

    Caching images

--- backend/src/http/routes/static.routes.ts
+++ backend/src/http/routes/static.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
 import { createRouter } from '@/bootstrap/configure';
+import { config } from '@/config';
 
 const staticRouter = createRouter();
 
-staticRouter.use(express.static('public'));
+staticRouter.use(
+  express.static('public', {
+    maxAge: config.sessions.maxAge,
+  }),
+);
 
 export { staticRouter };
