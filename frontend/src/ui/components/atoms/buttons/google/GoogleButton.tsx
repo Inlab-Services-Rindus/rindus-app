33940b550512640dc2207dd8d2805e4b0a352cdc
Adrian Gonzalez <adrian.gonzalez@rindus.de>
2024-03-04 14:50:13 +0100

    Revert "Move credentials / config"
    This reverts commit 87d555d11a702092faf9a2a880d3553d0637acd7.

--- frontend/src/ui/components/atoms/buttons/google/GoogleButton.tsx
+++ frontend/src/ui/components/atoms/buttons/google/GoogleButton.tsx
@@ -13,7 +13,8 @@ export default function GoogleButto({
     if (window?.google?.accounts?.id) {
       window?.google?.accounts?.id?.initialize({
         callback: afterLogin,
-        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+        client_id:
+          '794492959607-21m9v38tca8f0i957p9bk67li2g7nt9b.apps.googleusercontent.com',
       });
     }
 
