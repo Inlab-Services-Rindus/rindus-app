11ec7d22446a60928611d07e9876bde2f407bf0e
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-03-21 11:48:11 +0100

    RA-122 Add Events Button

--- frontend/src/ui/section/home/Home.spec.tsx
+++ frontend/src/ui/section/home/Home.spec.tsx
@@ -8,6 +8,9 @@ vi.mock('@/ui/section/home/people-tab/PeopleTab', () => ({
 vi.mock('@/ui/section/home/partners-tab/PartnersTab', () => ({
   default: () => <div data-testid="partners-tab">PartnersTab</div>,
 }));
+vi.mock('@/ui/section/home/events-tab/EventsTab', () => ({
+  default: () => <div data-testid="events-tab">EventsTab</div>,
+}));
 
 describe.skip('Home', () => {
   it('should render people tab by default', () => {
@@ -27,4 +30,16 @@ describe.skip('Home', () => {
     expect(screen.getByTestId('tab-panel')).toBeInTheDocument();
     expect(screen.getByTestId('partners-tab')).toBeInTheDocument();
   });
+
+  it('should render events tab when change the tab', () => {
+    render(<Home />);
+
+    act(() => {
+      screen.getAllByRole('button')[2].click();
+    });
+
+    expect(screen.getByTestId('tab-panel')).toBeInTheDocument();
+    expect(screen.getByTestId('events-tab')).toBeInTheDocument();
+  });
+
 });
---
+++ frontend/src/ui/section/home/events-tab/EventsTab.tsx
@@ -0,0 +1,5 @@
+
+
+export default function EventsTab() {
+    return <p> Hola </p>
+};
