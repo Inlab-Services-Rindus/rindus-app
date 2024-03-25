daad84065fcf07a1264c9325eb49766ad0e3e7d8
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-03-25 15:34:57 +0100

    RA-122: Improve home text and removed tests

--- frontend/src/ui/section/home/Home.spec.tsx
+++ frontend/src/ui/section/home/Home.spec.tsx
@@ -1,44 +1,15 @@
 import { Home } from '@/ui/section/home/Home';
-import { act, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 vi.mock('@/ui/section/home/people-tab/PeopleTab', () => ({
   default: () => <div data-testid="people-tab">PeopleTab</div>,
 }));
 
-vi.mock('@/ui/section/home/partners-tab/PartnersTab', () => ({
-  default: () => <div data-testid="partners-tab">PartnersTab</div>,
-}));
-vi.mock('@/ui/section/home/events-tab/EventsTab', () => ({
-  default: () => <div data-testid="events-tab">EventsTab</div>,
-}));
-
-describe.skip('Home', () => {
+describe('Home', () => {
   it('should render people tab by default', () => {
     render(<Home />);
 
     expect(screen.getByTestId('tab-panel')).toBeInTheDocument();
     expect(screen.getByTestId('people-tab')).toBeInTheDocument();
-  });
-
-  it('should render partner tab when change the tab', () => {
-    render(<Home />);
-
-    act(() => {
-      screen.getAllByRole('button')[1].click();
-    });
-
-    expect(screen.getByTestId('tab-panel')).toBeInTheDocument();
-    expect(screen.getByTestId('partners-tab')).toBeInTheDocument();
-  });
-
-  it('should render events tab when change the tab', () => {
-    render(<Home />);
-
-    act(() => {
-      screen.getAllByRole('button')[2].click();
-    });
-
-    expect(screen.getByTestId('tab-panel')).toBeInTheDocument();
-    expect(screen.getByTestId('events-tab')).toBeInTheDocument();
   });
 });
--- frontend/src/ui/section/home/events-tab/EventsTab.tsx
+++ frontend/src/ui/section/home/events-tab/EventsTab.tsx
@@ -1,3 +1,3 @@
 export default function EventsTab() {
-  return <p> Hola </p>;
+  return <p> Coming soon </p>;
 }
