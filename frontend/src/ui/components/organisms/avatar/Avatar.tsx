bff6308d9be70624aee3a191619afcb8c6a81eff
Cristina Aguilar <cristina.aguilar@rindus.de>
2024-04-10 11:29:31 +0200

    RA-135: added check to avatar

--- frontend/src/ui/components/organisms/avatar/Avatar.tsx
+++ frontend/src/ui/components/organisms/avatar/Avatar.tsx
@@ -1,5 +1,7 @@
 import cake from '@/assets/icons/Cake_24.svg';
 import captain from '@/assets/icons/Capitan_24.svg';
+import check from '@/assets/icons/Check.svg';
+
 import '@/ui/components/organisms/avatar/Avatar.scss';
 
 export type AvatarSize = 'small' | 'medium' | 'large';
@@ -8,6 +10,7 @@ interface AvatarProps {
   isBirthday?: boolean;
   isCaptain?: boolean;
   size?: AvatarSize;
+  isAttendance?: boolean;
 }
 
 export function Avatar({
@@ -15,6 +18,7 @@ export function Avatar({
   isCaptain,
   profilePictureUrl,
   size = 'medium',
+  isAttendance,
 }: AvatarProps): JSX.Element {
   function renderBadge() {
     if (isBirthday) {
@@ -33,6 +37,15 @@ export function Avatar({
           className={`avatar__badge avatar__badge--captain avatar__badge--${size}`}
         >
           <img src={captain} alt="captain" />
+        </div>
+      );
+    }
+    if (isAttendance) {
+      return (
+        <div
+          className={`avatar__badge avatar__badge--check avatar__badge--${size}`}
+        >
+          <img src={check} alt="check" />
         </div>
       );
     }
