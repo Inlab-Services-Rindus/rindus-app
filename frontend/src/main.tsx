dbdbb5fb6c5a9466dd11e83c4f76b0dc1bdb72a9
Pedro Domínguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2024-05-14 10:04:06 +0200

    Move analytics component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App.tsx';
+import { Analytics } from '@vercel/analytics/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <Analytics />
+    <App />
+  </>,
+);
