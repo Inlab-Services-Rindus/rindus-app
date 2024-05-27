8e0df75d924d41f52bcdfa8851492ee3d4063492
Adrian Gonzalez <adrian.gonzalez@rindus.de>
2024-05-27 15:43:00 +0200

    Test service worker

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App.tsx';
 import { Analytics } from '@vercel/analytics/react';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
