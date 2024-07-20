94ed3a6ac885ad16f1a06c5b0ae65d605973b7ec
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-07-20 10:35:45 +0200

    RA-177: Add Online Event Tag mock

--- frontend/src/mocks/event.ts
+++ frontend/src/mocks/event.ts
@@ -13,4 +13,5 @@ export const eventMock: DetailedEvent = {
     '\u003cp\u003e:tada: Welcome Lunch for New Team Members! :knife_fork_plate:\u003c/p\u003e\u003cp\u003eLet’s celebrate our awesome new colleagues with a delicious lunch! Join us for good food and great company as we welcome them to the team.\u003c/p\u003e',
   location: 'www.zoom.com',
   time: '13:00 - 15:00',
+  isOnlineEvent: true,
 };
