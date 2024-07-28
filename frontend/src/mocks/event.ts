ea03b3d403dd2844a56718937e5a859199969258
Antonio Morales <antonio.morales@rindus.de>
2024-07-28 23:41:48 +0200

    RA-178: update eventMock

--- frontend/src/mocks/event.ts
+++ frontend/src/mocks/event.ts
@@ -11,6 +11,6 @@ export const eventMock: DetailedEvent = {
   },
   description:
     '\u003cp\u003e:tada: Welcome Lunch for New Team Members! :knife_fork_plate:\u003c/p\u003e\u003cp\u003eLet’s celebrate our awesome new colleagues with a delicious lunch! Join us for good food and great company as we welcome them to the team.\u003c/p\u003e',
-  location: 'www.zoom.com',
+  locationUrl: 'www.zoom.com',
   time: '13:00 - 15:00',
 };
