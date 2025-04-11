c1cff0c6b0c8e9891a58b4cefa2f83ca2484f6f9
Pedro Dominguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2025-04-11 18:02:19 +0200

    RA-207: Create new migration for pins feature

---
+++ api/internal/repository/migrations/000008_create_pins_tables.down.sql
@@ -0,0 +1,4 @@
+-- Drop tables in reverse order to handle dependencies
+DROP TABLE IF EXISTS people_pins;
+DROP TABLE IF EXISTS pins;
+DROP TABLE IF EXISTS pins_category;
---
+++ api/internal/repository/migrations/000008_create_pins_tables.up.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS pins_category (
+    id serial NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pins_category_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS pins (
+    id serial NOT NULL,
+    event_date DATE NOT NULL,
+    image_pin TEXT NOT NULL,
+    pin_title VARCHAR(255) NOT NULL,
+    pin_description TEXT NOT NULL,
+    auto_assigned BOOLEAN DEFAULT false,
+    category_id INTEGER NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pins_pkey PRIMARY KEY (id),
+    CONSTRAINT pins_category_id_foreign FOREIGN KEY (category_id) REFERENCES pins_category(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS people_pins (
+    employee_id INTEGER NOT NULL,
+    pin_id INTEGER NOT NULL,
+    assignment_date DATE NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (employee_id, pin_id),
+    CONSTRAINT people_pins_pin_id_foreign FOREIGN KEY (pin_id) REFERENCES pins(id) ON DELETE CASCADE
+);
