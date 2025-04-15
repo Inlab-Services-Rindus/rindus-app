ad69c701eb9f8819c4de2da00dd81f05e816b8ea
Pedro Dominguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2025-04-15 22:44:29 +0200

    RA-207: Add soft delete field

--- api/internal/repository/migrations/000008_create_pins_tables.up.sql
+++ api/internal/repository/migrations/000008_create_pins_tables.up.sql
@@ -1,10 +1,13 @@
 CREATE TABLE IF NOT EXISTS pins_category (
     id serial NOT NULL,
     name VARCHAR(255) NOT NULL,
+    deleted_at timestamptz,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pins_category_pkey PRIMARY KEY (id)
 );
+
+CREATE INDEX idx_pins_category_deleted_at ON pins_category(deleted_at);
 
 CREATE TABLE IF NOT EXISTS pins (
     id serial NOT NULL,
@@ -14,17 +17,23 @@ CREATE TABLE IF NOT EXISTS pins (
     pin_description TEXT NOT NULL,
     auto_assigned BOOLEAN DEFAULT false,
     category_id INTEGER NOT NULL,
+    deleted_at timestamptz,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT pins_pkey PRIMARY KEY (id),
     CONSTRAINT pins_category_id_foreign FOREIGN KEY (category_id) REFERENCES pins_category(id) ON DELETE CASCADE
 );
 
+CREATE INDEX idx_pins_deleted_at ON pins(deleted_at);
+
 CREATE TABLE IF NOT EXISTS employee_pins (
     employee_id INTEGER NOT NULL,
     pin_id INTEGER NOT NULL,
+    deleted_at timestamptz,
     created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (employee_id, pin_id),
     CONSTRAINT employee_pins_pin_id_foreign FOREIGN KEY (pin_id) REFERENCES pins(id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_employee_pins_deleted_at ON employee_pins(deleted_at);
